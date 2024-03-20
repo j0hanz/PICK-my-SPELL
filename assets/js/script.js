b2a1629adddd666c4e349f21f0eb37d7702d3334
Linus Johansson <golfarenlj@gmail.com>
2024-03-20 08:36:26 +0000

    New repo. css, js structure added.

---
+++ assets/js/script.js
@@ -0,0 +1,540 @@
+/*
+Hard Questions
+*/
+
+let questionsHard = [
+    {
+      question: "fa-mosquito",
+      answers: [
+        { text: "mocquito", correct: false },
+        { text: "moscuito", correct: false },
+        { text: "mosscuito", correct: false },
+        { text: "mossquito", correct: false },
+        { text: "mosquito", correct: true },
+        { text: "mosquitu", correct: false },
+      ],
+    },
+    {
+      question: "fa-bacterium",
+      answers: [
+        { text: "bacterum", correct: false },
+        { text: "bactearun", correct: false },
+        { text: "bacterium", correct: true },
+        { text: "baktearium", correct: false },
+        { text: "bacteriun", correct: false },
+        { text: "bakterium", correct: false },
+      ],
+    },
+    {
+      question: "fa-walkie-talkie",
+      answers: [
+        { text: "walke-talke", correct: false },
+        { text: "walki-talki", correct: false },
+        { text: "walkie-talki", correct: false },
+        { text: "walkie-talkie", correct: true },
+        { text: "walki-talkie", correct: false },
+        { text: "walke-talkie", correct: false },
+      ],
+    },
+    {
+      question: "fa-screwdriver-wrench",
+      answers: [
+        { text: "screw-driver-wrensh", correct: false },
+        { text: "screew-wrench", correct: false },
+        { text: "screw-drivewrench", correct: false },
+        { text: "screwdriver-wensch", correct: false },
+        { text: "screew-driverwrench", correct: false },
+        { text: "screwdriver-wrench", correct: true },
+      ],
+    },
+    {
+      question: "fa-hanukiah",
+      answers: [
+        { text: "hanukiah", correct: true },
+        { text: "hanukah", correct: false },
+        { text: "hanukeah", correct: false },
+        { text: "hannucia", correct: false },
+        { text: "hanucia", correct: false },
+        { text: "hanuciah", correct: false },
+      ],
+    },
+    {
+      question: "fa-synagogue",
+      answers: [
+        { text: "synagoge", correct: false },
+        { text: "synagugue", correct: false },
+        { text: "syndagoge", correct: false },
+        { text: "syndagogue", correct: false },
+        { text: "synagogue", correct: true },
+        { text: "synagouge", correct: false },
+      ],
+    },
+    {
+      question: "fa-thermometer",
+      answers: [
+        { text: "termometer", correct: false },
+        { text: "thernometer", correct: false },
+        { text: "thermometer", correct: true },
+        { text: "thermoneter", correct: false },
+        { text: "termoneter", correct: false },
+        { text: "ternometer", correct: false },
+      ],
+    },
+    {
+      question: "fa-calculator",
+      answers: [
+        { text: "calcolator", correct: false },
+        { text: "calkulator", correct: false },
+        { text: "calculetor", correct: false },
+        { text: "calculator", correct: true },
+        { text: "calclator", correct: false },
+        { text: "calcletor", correct: false },
+      ],
+    },
+    {
+      question: "fa-puzzle-piece",
+      answers: [
+        { text: "puzle-piece", correct: false },
+        { text: "puzzel-pice", correct: false },
+        { text: "puzzle-peace", correct: false },
+        { text: "puzzel-peace", correct: false },
+        { text: "puzzle-piece", correct: true },
+        { text: "puzzel-piece", correct: false },
+      ],
+    },
+    {
+      question: "fa-magnifying-glass",
+      answers: [
+        { text: "magnifying-glass", correct: true },
+        { text: "magniflying-glass", correct: false },
+        { text: "magnefying-glass", correct: false },
+        { text: "magnifyin-glass", correct: false },
+        { text: "magnifynin-glass", correct: false },
+        { text: "magni-fying-glass", correct: false },
+      ],
+    },
+  ];
+  
+  /*
+  Easy Questions
+  */
+  
+  let questionsEasy = [
+    {
+      question: "fa-headphones",
+      answers: [
+        { text: "Headphoens", correct: false },
+        { text: "Headphones", correct: true },
+        { text: "Headfones", correct: false },
+        { text: "Headfhones", correct: false },
+      ],
+    },
+    {
+      question: "fa-church",
+      answers: [
+        { text: "Church", correct: true },
+        { text: "Chirch", correct: false },
+        { text: "Chursh", correct: false },
+        { text: "Churh", correct: false },
+      ],
+    },
+    {
+      question: "fa-bell",
+      answers: [
+        { text: "Bel", correct: false },
+        { text: "Beel", correct: false },
+        { text: "Bell", correct: true },
+        { text: "Beal", correct: false },
+      ],
+    },
+    {
+      question: "fa-hippo",
+      answers: [
+        { text: "Hipo", correct: false },
+        { text: "Heppo", correct: false },
+        { text: "Hipoo", correct: false },
+        { text: "Hippo", correct: true },
+      ],
+    },
+    {
+      question: "fa-share",
+      answers: [
+        { text: "Shear", correct: false },
+        { text: "Shere", correct: false },
+        { text: "Share", correct: true },
+        { text: "Sehre", correct: false },
+      ],
+    },
+    {
+      question: "fa-compass",
+      answers: [
+        { text: "Compass", correct: true },
+        { text: "Compas", correct: false },
+        { text: "Commpas", correct: false },
+        { text: "Comppas", correct: false },
+      ],
+    },
+    {
+      question: "fa-brush",
+      answers: [
+        { text: "Brusch", correct: false },
+        { text: "Bruch", correct: false },
+        { text: "Brosh", correct: false },
+        { text: "Brush", correct: true },
+      ],
+    },
+    {
+      question: "fa-handshake",
+      answers: [
+        { text: "Hand shake", correct: false },
+        { text: "Handchake", correct: false },
+        { text: "Handchack", correct: false },
+        { text: "Handshake", correct: true },
+      ],
+    },
+    {
+      question: "fa-snowflake",
+      answers: [
+        { text: "Snowflace", correct: false },
+        { text: "Snowflaek", correct: false },
+        { text: "Snowflake", correct: true },
+        { text: "Snow-flake", correct: false },
+      ],
+    },
+    {
+      question: "fa-feather",
+      answers: [
+        { text: "Feether", correct: false },
+        { text: "Featcher", correct: false },
+        { text: "Feethear", correct: false },
+        { text: "Feather", correct: true },
+      ],
+    },
+    {
+      question: "fa-paperclip",
+      answers: [
+        { text: "Paperclip", correct: true },
+        { text: "Papperclip", correct: false },
+        { text: "Paperklip", correct: false },
+        { text: "Papperklip", correct: false },
+      ],
+    },
+    {
+      question: "fa-umbrella",
+      answers: [
+        { text: "Ummbrela", correct: false },
+        { text: "Ummbrella", correct: false },
+        { text: "Umbrela", correct: false },
+        { text: "Umbrella", correct: true },
+      ],
+    },
+    {
+      question: "fa-trash",
+      answers: [
+        { text: "Trasch", correct: false },
+        { text: "Trach", correct: false },
+        { text: "Trash", correct: true },
+        { text: "Tratch", correct: false },
+      ],
+    },
+    {
+      question: "fa-bicycle",
+      answers: [
+        { text: "Bicycle", correct: true },
+        { text: "Bisycle", correct: false },
+        { text: "Bioycle", correct: false },
+        { text: "Biqycle", correct: false },
+      ],
+    },
+    {
+      question: "fa-school",
+      answers: [
+        { text: "Shool", correct: false },
+        { text: "Schol", correct: false },
+        { text: "School", correct: true },
+        { text: "Scholl", correct: false },
+      ],
+    },
+    {
+      question: "fa-motorcycle",
+      answers: [
+        { text: "Motorcykle", correct: false },
+        { text: "Motorcycl", correct: false },
+        { text: "Motorcyqle", correct: false },
+        { text: "Motorcycle", correct: true },
+      ],
+    },
+    {
+      question: "fa-hammer",
+      answers: [
+        { text: "Hammer", correct: true },
+        { text: "Hamer", correct: false },
+        { text: "Hammear", correct: false },
+        { text: "Hemmar", correct: false },
+      ],
+    },
+    {
+      question: "fa-repeat",
+      answers: [
+        { text: "Rapeat", correct: false },
+        { text: "Reapet", correct: false },
+        { text: "Repeet", correct: false },
+        { text: "Repeat", correct: true },
+      ],
+    },
+    {
+      question: "fa-microphone",
+      answers: [
+        { text: "Microphone", correct: true },
+        { text: "Microphoen", correct: false },
+        { text: "Microfone", correct: false },
+        { text: "Micqrophone", correct: false },
+      ],
+    },
+    {
+      question: "fa-spoon",
+      answers: [
+        { text: "Spoone", correct: false },
+        { text: "Spoun", correct: false },
+        { text: "Spoon", correct: true },
+        { text: "Spoen", correct: false },
+      ],
+    },
+  ];
+  
+  // Variables
+  
+  let playButton = document.getElementById("play-btn");
+  
+  let howToPlay = document.getElementById("guide-btn");
+  
+  let feedbackButton = document.getElementById("feedback-btn");
+  
+  let showGameGuide = document.getElementById("how-to-play");
+  
+  let showFeedback = document.getElementById("feedback");
+  
+  let closeButton = document.getElementById("close-btn");
+  
+  let easyButton = document.getElementById("easy-btn");
+  
+  let hardButton = document.getElementById("hard-btn");
+  
+  let quitButton = document.getElementById("quit-btn");
+  
+  let gameElement = document.getElementById("game-area");
+  
+  let questionElement = document.getElementById("question");
+  
+  let changeDifficulty = document.getElementById("hard");
+  
+  let socialMedia = document.getElementById("socialMedia");
+  
+  let answerButtonsElement = document.getElementById("answer-buttons");
+  
+  let shuffledQuestions, currentQuestionIndex;
+  
+  let icons = document.getElementById("icons");
+  
+  let countdownElement = document.getElementById("countdown");
+  
+  let headerElement = document.getElementById("header");
+  
+  let score;
+  
+  let wrongAnswer;
+  
+  let timeLeft;
+  
+  let timerInterval;
+  
+  // Enables Start & Quit button
+  playButton.addEventListener("click", playGame);
+  howToPlay.addEventListener("click", showGuide);
+  feedbackButton.addEventListener("click", feedback);
+  easyButton.addEventListener("click", startGameEasy);
+  hardButton.addEventListener("click", startGameHard);
+  quitButton.addEventListener("click", quitGame);
+  closeButton.addEventListener("click", closeText);
+  
+  function showGuide() {
+    showGameGuide.classList.remove("hide");
+    showFeedback.classList.add("hide");
+    closeButton.classList.remove("hide");
+    playButton.classList.add("hide");
+    socialMedia.classList.add("hide");
+    howToPlay.classList.add("hide");
+    feedbackButton.classList.add("hide");
+  }
+  
+  function feedback() {
+    showGameGuide.classList.add("hide");
+    showFeedback.classList.remove("hide");
+    closeButton.classList.remove("hide");
+    playButton.classList.add("hide");
+    socialMedia.classList.add("hide");
+    howToPlay.classList.add("hide");
+    feedbackButton.classList.add("hide");
+  }
+  
+  function closeText() {
+    if (confirm) {
+      window.location = "index.html";
+    }
+  }
+  
+  function playGame() {
+    easyButton.classList.remove("hide");
+    hardButton.classList.remove("hide");
+    playButton.classList.add("hide");
+    socialMedia.classList.add("hide");
+    howToPlay.classList.add("hide");
+    showGameGuide.classList.add("hide");
+    showFeedback.classList.add("hide");
+    feedbackButton.classList.add("hide");
+  }
+  
+  function startTimer() {
+    timeLeft = 10;
+    document.getElementById("countdown").textContent = timeLeft;
+    clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+      timeLeft--;
+      document.getElementById("countdown").textContent = timeLeft;
+      if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        wrongAnswer++;
+        document.getElementById("wrong").textContent = wrongAnswer;
+        if (currentQuestionIndex < shuffledQuestions.length - 1) {
+          currentQuestionIndex++;
+          nextQuestion();
+          startTimer();
+        }
+      }
+    }, 1000);
+  }
+  
+  //Start game Easy
+  
+  function startGameEasy() {
+    startTimer();
+    score = 0;
+    wrongAnswer = 0;
+    document.getElementById("correct").innerText = `${score}`;
+    document.getElementById("wrong").innerText = `${wrongAnswer}`;
+    easyButton.classList.add("hide");
+    headerElement.classList.add("hide");
+    hardButton.classList.add("hide");
+    showGameGuide.classList.add("hide");
+    shuffledQuestions = questionsEasy.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    gameElement.classList.remove("hide");
+    nextQuestion();
+  }
+  //Start game Hard
+  function startGameHard() {
+    startTimer();
+    score = 0;
+    wrongAnswer = 0;
+    document.getElementById("correct").innerText = `${score}`;
+    document.getElementById("wrong").innerText = `${wrongAnswer}`;
+    easyButton.classList.add("hide");
+    headerElement.classList.add("hide");
+    hardButton.classList.add("hide");
+    showGameGuide.classList.add("hide");
+    shuffledQuestions = questionsHard.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    gameElement.classList.remove("hide");
+    nextQuestion();
+  }
+  
+  function quitGame() {
+    if (confirm) {
+      window.location = "index.html";
+    }
+    resetState();
+  }
+  
+  function nextQuestion() {
+    resetState();
+  
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  
+    startTimer();
+  }
+  
+  function showQuestion(question) {
+    //questionElement.innerText = question.question;
+    questionElement.classList.add(question.question);
+    quitButton.classList.remove("hide");
+  
+    question.answers.forEach((answer) => {
+      let button = document.createElement("button");
+  
+      button.innerText = answer.text;
+  
+      button.classList.add("btn");
+  
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+  
+      button.addEventListener("click", selectAnswer);
+  
+      answerButtonsElement.appendChild(button);
+    });
+  }
+  
+  function resetState() {
+    clearStatus(document.body);
+  
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+  }
+  
+  function selectAnswer(e) {
+    // Get the selected answer
+    let selectedButton = e.target;
+    // Check if the selected answer is correct
+    let correct = selectedButton.dataset.correct;
+  
+    // When the answer is correct
+    if (correct) {
+      score++;
+      document.getElementById("correct").innerText = `${score}`;
+      currentQuestionIndex++;
+    } else {
+      // When the answer is wrong
+      wrongAnswer++;
+      document.getElementById("wrong").innerText = `${wrongAnswer}`;
+      currentQuestionIndex++;
+    }
+    setStatus(document.body, correct);
+  
+    Array.from(answerButtonsElement.children).forEach((button) => {
+      setStatus(button, button.dataset.correct);
+    });
+  
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    } else {
+    }
+  }
+  
+  function setStatus(element, correct) {
+    clearStatus(element);
+  
+    if (correct) {
+      element.classList.add("correct");
+      nextQuestion();
+    } else {
+      element.classList.add("wrong");
+      nextQuestion();
+    }
+  }
+  
+  function clearStatus(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+  }
+  
